934a6844752349f0937a9027bd44196e4cc39349
kartikey012 <kartikeyrauniyar0@gmail.com>
2026-05-06 13:19:13 +0530

    Add files via upload

---
+++ js_type.js
@@ -0,0 +1,10 @@
+document.write("<h1 style='color:red;'>Kartikey Rauniyar<h1><br>");
+        document.write("<h1 style='color:blue;'>Btech CSE Core<br>");
+        document.write("<h1 style='color:green;'>Playing vollyball<br>");
+        document.write("<h1 style='color:yellow;'>Currently feeling sleepy<br>"); //for printing we use java script
+
+        // function T1(){
+        //     document.write("Welcome");
+        //     document.alert("registered");
+        // }
+        console.log("Welcome to my world ");
